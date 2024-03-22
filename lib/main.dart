9421ba6ec70a0e59dfa3a13e393eb78a4662f7e9
ASUS <5646krupaparmar@gmail.com>
2024-03-22 21:48:42 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,188 @@
+//
+// TODO -> TASK 1 -LIST OF FRUITS
+//
+//
+
+// import 'package:flutter/material.dart';
+//
+// void main()
+// {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.teal,
+//           centerTitle: true,
+//           title: Text('🛍️List of Friuts',style: TextStyle(fontSize: 18,color: Colors.white,fontWeight: FontWeight.bold,letterSpacing: 2),),
+//
+//         ),
+//         body: const Center(
+//             child: Text.rich(TextSpan(
+//                 children: [
+//                   TextSpan(text: '🍎  Apple\n',style: TextStyle(fontSize: 26,color: Colors.red,)),
+//                   TextSpan(text: '🍇  Greps\n',style: TextStyle(fontSize: 26,color: Colors.purpleAccent)),
+//                   TextSpan(text: '🍒  Cherry\n',style: TextStyle(fontSize: 26,color: Colors.purple)),
+//                   TextSpan(text: '🍓  Strawberry\n',style: TextStyle(fontSize: 26,color: Colors.pink)),
+//                   TextSpan(text: ' 🥭    Mango\n',style: TextStyle(fontSize: 26,color: Colors.amber)),
+//                   TextSpan(text: '🍍  Pineapple\n',style: TextStyle(fontSize: 26,color: Colors.green)),
+//                   TextSpan(text: '🍋  Lemon\n',style: TextStyle(fontSize: 26,color: Colors.amberAccent)),
+//                   TextSpan(text: '🍉  Watermelon\n',style: TextStyle(fontSize: 26,color: Colors.lightGreen)),
+//                   TextSpan(text: '🥥  Coconut\n\n',style: TextStyle(fontSize: 26,color: Colors.brown)),
+//                 ]
+//             ))
+//
+//
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+
+
+
+// //TODO -> TASK 2 -RED & WHITE (GRAPHICS,FLUTTER.....)
+
+//
+// import 'package:flutter/material.dart';
+//
+// void main()
+// {
+//   runApp(
+//     MaterialApp(
+//
+//       debugShowCheckedModeBanner: false,
+//
+//       title: 'krupa parmar',
+//
+//       home: Scaffold(
+//         backgroundColor: Colors.black,
+//         appBar: AppBar(
+//           backgroundColor: Colors.red,
+//           centerTitle: true,
+//           title:Text('Red & White',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+//         ),
+//         body: const Center(
+//           child: Text.rich(
+//             TextSpan(
+//               children: [
+//                 TextSpan(text: '            G',style: TextStyle(color: Colors.green,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'R',style: TextStyle(color: Colors.red,fontSize: 28,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'APHICS\n',style: TextStyle(color: Colors.green,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//
+//                 TextSpan(text: '     FLUTT',style: TextStyle(color: Colors.blue,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'E',style: TextStyle(color: Colors.red,fontSize: 28,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'R\n',style: TextStyle(color: Colors.blue,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//
+//                 TextSpan(text: '          AN',style: TextStyle(color: Colors.green,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'D',style: TextStyle(color: Colors.red,fontSize: 28,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'ROID\n',style: TextStyle(color: Colors.green,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//
+//                 TextSpan(text: ' DESIGN',style: TextStyle(color: Colors.amber,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: '  & ',style: TextStyle(color: Colors.red,fontSize: 28,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'DEVELOP\n',style: TextStyle(color: Colors.amber,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//
+//                 TextSpan(text: '            W',style: TextStyle(color: Colors.red,fontSize: 28,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'EB\n',style: TextStyle(color: Colors.blue,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//
+//                 TextSpan(text: '         FAS',style: TextStyle(color: Colors.lightGreenAccent,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'H',style: TextStyle(color: Colors.red,fontSize: 28,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'ION\n',style: TextStyle(color: Colors.lightGreenAccent,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//
+//                 TextSpan(text: '   ANIMAT',style: TextStyle(color: Colors.teal,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'I',style: TextStyle(color: Colors.red,fontSize: 28,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'ON\n',style: TextStyle(color: Colors.teal,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//
+//                 TextSpan(text: '              I',style: TextStyle(color: Colors.blue,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'T',style: TextStyle(color: Colors.red,fontSize: 28,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'A-CS+\n',style: TextStyle(color: Colors.blue,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//
+//                 TextSpan(text: '        GAM',style: TextStyle(color: Colors.amber,fontSize: 22,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//                 TextSpan(text: 'E',style: TextStyle(color: Colors.red,fontSize: 28,letterSpacing: 3,fontWeight: FontWeight.bold)),
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+
+// TODO -> TASK - 3 ->FLUTTER RICH TEXT (HIGHLIGHTED)
+//
+// import 'package:flutter/material.dart';
+//
+// void main()
+// {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.blue,
+//           title:Text('Flutter RichText',style: TextStyle(color: Colors.white,fontSize: 18),),
+//         ),
+//         body: const Center(
+//           child: Text.rich(
+//             TextSpan(
+//                 children: [
+//                   TextSpan(text: 'Rich Text helps to create\n',style: TextStyle(color: Colors.black38,fontSize: 20)),
+//                   TextSpan(text: 'Highlighted',style: TextStyle(fontSize: 40,color: Colors.red,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold)),
+//                   TextSpan(text: ',\n',style: TextStyle(color: Colors.black38,fontSize: 20)),
+//                   TextSpan(text: 'Clickable ',style: TextStyle(color: Colors.blue,fontSize: 20,decoration: TextDecoration.underline,decorationColor: Colors.blue)),
+//                   TextSpan(text: ',',style: TextStyle(color: Colors.black38,fontSize: 20,)),
+//                   TextSpan(text: 'OutlinedText\n',style: TextStyle(color: Colors.green,fontSize: 20,)),
+//                   TextSpan(text: '  Say Hi to Rich Text',style: TextStyle(color: Colors.black38,fontSize: 22)),
+//                 ]
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+
+//TODO -> TASK - 4 ->RICH TEXT EXAMPLE ( SINGLE LINE MULTIPLE STYLE)
+
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+
+      home: Scaffold(
+
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+
+          title: Text('Rich Text Example',style: TextStyle(color: Colors.white),),
+        ),
+        body: const Center(
+          child:Text.rich(
+              TextSpan( children: [
+                TextSpan(text: 'single ',style: TextStyle(color: Colors.blue,fontSize: 20,fontWeight: FontWeight.bold,)),
+                TextSpan(text: ' Line ',style: TextStyle(color: Colors.white,backgroundColor: Colors.teal,fontSize: 18,fontWeight: FontWeight.bold)),
+                TextSpan(text: ' Multiple',style: TextStyle(color: Colors.deepOrangeAccent,fontWeight: FontWeight.normal,fontSize: 18,letterSpacing: 2,fontFamily: 'monostice')),
+                TextSpan(text: ' styles',style: TextStyle(color: Colors.pink,fontSize: 20,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold)),
+
+              ]
+              )
+          ),
+        ),
+      ),
+    ),
+  );
+}
